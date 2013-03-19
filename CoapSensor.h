3659163c366ae296841c802c9c7b3585d1cdd0a3
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-03-19 13:37:54 +0200

    commit license

--- CoapSensor.h
+++ CoapSensor.h
@@ -1,5 +1,22 @@
-#ifndef COAP_SENSOR_H
-#define COAP_SENSOR_H
+/********************************************************************************
+ ** The Arduino-CoAP is free software: you can redistribute it and/or modify   **
+ ** it under the terms of the GNU Lesser General Public License as             **
+ ** published by the Free Software Foundation, either version 3 of the         **
+ ** License, or (at your option) any later version.                            **
+ **                                                                            **
+ ** The Arduino-CoAP is distributed in the hope that it will be useful,        **
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of             **
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              **
+ ** GNU Lesser General Public License for more details.                        **
+ **                                                                            **
+ ** You should have received a copy of the GNU Lesser General Public           **
+ ** License along with the Arduino-CoAP.                                       **
+ ** If not, see <http://www.gnu.org/licenses/>.                                **
+ *******************************************************************************/
+
+#ifndef COAPSENSOR_H
+#define COAPSENSOR_H
+
 #include "packet.h"
 
 class CoapSensor {
