a17c3e8386105f99d985c41fc9832216b6a9b5b0
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-06-12 15:11:47 +0300

    change conf to more realistic values

--- coap_conf.h
+++ coap_conf.h
@@ -17,11 +17,11 @@
 #define WISELIB_MID_COAP                    51
 // end of wiselib defines
 // CONFIGURATION
-#define CONF_MAX_RESOURCES                  10
-#define CONF_LARGE_BUF_LEN                  CONF_MAX_RESOURCES*10
-#define CONF_HELPER_BUF_LEN                 CONF_MAX_RESOURCES*10
+#define CONF_MAX_RESOURCES                  9
+#define CONF_LARGE_BUF_LEN                  CONF_MAX_RESOURCES*6
+#define CONF_HELPER_BUF_LEN                 CONF_MAX_RESOURCES*6
 #define CONF_MAX_RESOURCE_QUERIES           1
-#define CONF_MAX_OBSERVERS                  10
+#define CONF_MAX_OBSERVERS                  9
 #define CONF_MAX_MSG_LEN                    99
 #define CONF_MAX_PAYLOAD_LEN                64
 #define CONF_PIGGY_BACKED                   1
